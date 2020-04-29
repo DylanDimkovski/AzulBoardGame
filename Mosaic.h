bb5941040e4620001fb6e1786a217e163884ded7
[Michael Sartorel s3786267] <[s3786267@student.rmit.edu.au]>
2020-04-29 13:57:21 +1000

    Implemented Mosaic class

---
+++ Mosaic.h
@@ -0,0 +1,25 @@
+#ifndef MOSIAC_H
+#define MOSIAC_H
+
+#include "Types.h"
+#include "Factory.h"
+#include "Line.h"
+#include "TileList.h"
+
+class Mosaic
+{
+    public:
+        Mosaic();
+        ~Mosaic();
+        bool isFilled(int row, int col);
+        void setFilled(int row, int col, bool filled);
+        void insertTilesIntoLine(int row, int quantity, TileType tileType);
+
+    private:
+        Line* lines[NUMBER_OF_LINES];
+        bool wall[NUMBER_OF_LINES][NUMBER_OF_LINES];
+        // may need to be pointer
+        TileList brokenTiles;
+};
+
+#endif
--- Types.h
+++ Types.h
@@ -1,8 +1,7 @@
 #ifndef TYPES_H
 #define TYPES_H
 
-#define WALL_SIZE 5
-#define LINE_SIZE 5
+#define NUMBER_OF_LINES 5
 #define BROKEN_TILES_LENGTH 7
 #define NUMBER_OF_PLAYERS 2
 #define NUMBER_OF_EACH_COLOUR 20
