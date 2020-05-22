414014680380c99416b1f56ea7ffc2b7e76bcf95
[Michael Sartorel s3786267] <[s3786267@student.rmit.edu.au]>
2020-05-22 13:18:29 +1000

    Added tile quantity checking in load method

--- Saver.h
+++ Saver.h
@@ -23,7 +23,7 @@ class Saver
         void save(GameEngine* gameEngine, std::ofstream& outputStream);
         GameEngine* load(std::istream& inputStream, Menu* menu);
         void outputWall(std::ofstream& outputStream, Mosaic* mosaic);
-        Mosaic* generateMosiac(std::string lines[SAVE_FILE_LINES_LENGTH], int startingLine);
+        Mosaic* generateMosiac(std::string lines[SAVE_FILE_LINES_LENGTH], int startingLine, bool& addedFirstTile, int& tileCount);
         void cleanUpFactories(Factory* factories[]);
         bool isValidFactory(TileType tiles[]);
 };
